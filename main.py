5d4cc009b064c17c67073198ec3c9fa19c44583e
Ritesh Kumar Verma <ritesh782002@gmail.com>
2026-09-25 21:32:46 +0530

    creating v2

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
+def main():
+    print("Hello from sentinel!")
 
 
-import streamlit as st
-from src.pages.Test_Display import display_rtsp_video
-from dotenv import load_dotenv
-from src.pages.Home import Home_UI
-import os
-load_dotenv()
-
-st.set_page_config(
-    page_title="Sentinel",
-    page_icon="🛡️",
-    layout="wide"
-)
-
-home = Home_UI(rstp_url=os.getenv("RTSP_URL"))
-
-
-
-
-# display_rtsp_video(os.getenv("RTSP_URL"))
+if __name__ == "__main__":
+    main()
